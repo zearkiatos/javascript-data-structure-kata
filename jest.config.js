7b8467aeb284f896820866df43cae2553aa88e57
zearkiatos <caprilespe@outlook.com>
2023-07-28 20:52:05 -0400

    Quit coverage folder

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,10 @@ module.exports = {
     collectCoverageFrom: ["src/**/*.js", "!**/node_modules/**"],
     coverageThreshold: {
       global: {
-        statements: 0,
-        branches: 0,
-        lines: 0,
-        functions: 0,
+        statements: 100,
+        branches: 100,
+        lines: 100,
+        functions: 100,
       },
     },
   };

13371c6c23e30afc0fc14c3eb4e492ae3ee4b23c
zearkiatos <caprilespe@outlook.com>
2023-08-06 17:18:37 -0400

    Add node to the list

--- src/linkedList/SinglyLinkedList.js
+++ src/linkedList/SinglyLinkedList.js
@@ -1,11 +1,25 @@
+import Node from "./Node";
 class SinglyLinkedList {
   constructor(value) {
-    this.head = {
-      value,
-      next: null,
-    };
+    this.head = new Node(value);
     this.tail = this.head;
     this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length += 1;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
   }
 }
 
--- tests/linkedList/Singly.unit.test.js
+++ tests/linkedList/Singly.unit.test.js
@@ -1,4 +1,5 @@
 import SinglyLinkedList from "../../src/linkedList/SinglyLinkedList";
+import Node from '../../src/linkedList/Node'
 describe("Unit test suite for Singly linked list", () => {
   test("Should create a singly linked list instance", () => {
     const expectedData = {
@@ -10,5 +11,64 @@ describe("Unit test suite for Singly linked list", () => {
     expect(singlyLinkedList.head).toEqual(expectedData);
     expect(singlyLinkedList.tail).toEqual(expectedData);
     expect(singlyLinkedList.length).toBe(1);
+  });
+
+  test("Should add a new element into the singly linked list", () => {
+    const expectedTail = new Node(5);
+    const expectedHead = {
+      value: 20,
+      next: {
+        value: 5,
+        next: expectedTail
+      }
+    };
+    expectedHead.next = expectedTail;
+    const singlyLinkedList = new SinglyLinkedList(20);
+    singlyLinkedList.append(5);
+
+    expect(singlyLinkedList.head).toEqual(expectedHead);
+    expect(singlyLinkedList.tail).toEqual(expectedTail);
+    expect(singlyLinkedList.length).toBe(2);
+  });
+
+  test("Should add two new elements", () => {
+    const expectedTail = new Node(21);
+    const expectedHead = new Node(20);
+    expectedHead.next = new Node(5);
+    expectedHead.next.next = new Node(21);
+    
+    const singlyLinkedList = new SinglyLinkedList(20);
+    singlyLinkedList.append(5);
+    singlyLinkedList.append(21);
+
+    expect(singlyLinkedList.head).toEqual(expectedHead);
+    expect(singlyLinkedList.tail).toEqual(expectedTail);
+    expect(singlyLinkedList.length).toBe(3);
+  });
+
+  test("Should add a new element in the head", () => {
+    const expectedHead = new Node(5);
+    const expectedTail = new Node(20);
+    expectedHead.next = expectedTail;
+    const singlyLinkedList = new SinglyLinkedList(20);
+    singlyLinkedList.prepend(5);
+
+    expect(singlyLinkedList.head).toEqual(expectedHead);
+    expect(singlyLinkedList.tail).toEqual(expectedTail);
+    expect(singlyLinkedList.length).toBe(2);
+  });
+
+  test("Should add two elements in the head", () => {
+    const expectedHead = new Node(10);
+    const expectedTail = new Node(20);
+    expectedHead.next = new Node(5);
+    expectedHead.next.next = new Node(20);
+    const singlyLinkedList = new SinglyLinkedList(20);
+    singlyLinkedList.prepend(5);
+    singlyLinkedList.prepend(10);
+
+    expect(singlyLinkedList.head).toEqual(expectedHead);
+    expect(singlyLinkedList.tail).toEqual(expectedTail);
+    expect(singlyLinkedList.length).toBe(3);
   });
 });
